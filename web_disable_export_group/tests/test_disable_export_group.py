0d73ff500a606128197e629c2ac4499eb37ecfa3
Andrius Laukavičius <andrius@focusate.eu>
2020-04-28 12:47:54 +0300

    [REF] web_disable_export_group: web_dashboard patch
    If web_disable_export_group is used with Odoo enterprise, we must also
    patch web_dashboard module, because it also overrides session_info.

--- web_disable_export_group/tests/test_disable_export_group.py
+++ web_disable_export_group/tests/test_disable_export_group.py
@@ -12,11 +12,14 @@ class TestDisableExportGroup(SavepointCase):
     @patch('odoo.addons.web_disable_export_group.models.ir_http.request')
     @patch('odoo.addons.web.models.ir_http.request')
     @patch('odoo.addons.web_tour.models.ir_http.request')
-    def test_session_info(self, request, request1, request2, request3):
+    @patch('odoo.addons.web_dashboard.models.ir_http.request')
+    def test_session_info(
+            self, request, request1, request2, request3, request4):
         request.env = self.env
         request1.env = self.env
         request2.env = self.env
         request3.env = self.env
+        request4.env = self.env
         session_info = self.env['ir.http'].session_info()
         self.assertTrue(session_info['group_export_data'])
 
@@ -24,8 +27,9 @@ class TestDisableExportGroup(SavepointCase):
     @patch('odoo.addons.web_disable_export_group.models.ir_http.request')
     @patch('odoo.addons.web.models.ir_http.request')
     @patch('odoo.addons.web_tour.models.ir_http.request')
-    def test_session_info_not_allowed(self, request, request1, request2,
-                                      request3):
+    @patch('odoo.addons.web_dashboard.models.ir_http.request')
+    def test_session_info_not_allowed(
+            self, request, request1, request2, request3, request4):
         demo_env = Environment(
             self.env.cr,
             self.env.ref('base.default_user').id,
@@ -35,5 +39,6 @@ class TestDisableExportGroup(SavepointCase):
         request1.env = demo_env
         request2.env = demo_env
         request3.env = demo_env
+        request4.env = demo_env
         session_info = demo_env['ir.http'].session_info()
         self.assertFalse(session_info['group_export_data'])
